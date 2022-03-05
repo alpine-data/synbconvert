c9d46e92753980902ec18c57f797640722a46d05
Michael Wellner <michaelwellner@kpmg.com>
2022-03-05 14:54:52 +0100

    Added initial BDD tests.

---
+++ features/environment.py
@@ -0,0 +1,18 @@
+from behave import *
+from behave.log_capture import capture
+
+import tempfile
+import shutil
+
+@capture()
+def before_scenario(context, scenario):
+    context.working_directory = tempfile.mkdtemp()
+
+    # prepare some lists to store mentioned entities during steps
+    context.cells = []
+    context.files = []
+    context.notebooks = []
+
+@capture()
+def after_scenario(context, scenario):
+    shutil.rmtree(context.working_directory)
